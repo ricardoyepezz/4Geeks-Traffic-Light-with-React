80440581368114c2fc1e97bb4a126226aa54829b
ricardoyepezz <ricardoyepezz@gmail.com>
2022-03-12 14:21:06 +0000

    add purple light

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -6,22 +6,26 @@ export const TrafficLight = () => {
 	const [color, setColor] = useState("");
 
 	return (
-		<div className="box">
-			<div
-				onClick={() => setColor("red")}
-				className={
-					"light red" + (color === "red" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setColor("yellow")}
-				className={
-					"light yellow" + (color === "yellow" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setColor("green")}
-				className={
-					"light green" + (color === "green" ? " glow" : "")
-				}></div>
-		</div>
+		<>
+			<div className="box">
+				<div
+					onClick={() => setColor("red")}
+					className={
+						"light red" + (color === "red" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setColor("yellow")}
+					className={
+						"light yellow" + (color === "yellow" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setColor("green")}
+					className={
+						"light green" + (color === "green" ? " glow" : "")
+					}></div>
+				<div className={color === "purple" ? " purple" : ""}></div>
+				<button onClick={() => setColor("purple")}>Purple</button>
+			</div>
+		</>
 	);
 };
